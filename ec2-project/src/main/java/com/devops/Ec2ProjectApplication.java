5ea408a11d6365da29afaea40839ac446b7796c6
Yash Sharma <yash32860@gmail.com>
2025-03-11 17:53:55 +0530

    how to run java application on ec2

---
+++ ec2-project/src/main/java/com/devops/Ec2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2ProjectApplication.class, args);
+	}
+
+}
---
+++ ec2-project/src/test/java/com/devops/Ec2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
